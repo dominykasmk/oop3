6f49808536510713f9d3b7b1f38e7588edfe415e
dominykasmk <dominykas.makunas@mif.stud.vu.lt>
2022-02-17 22:05:33 +0200

    implementinau pažymių įvedimą ir vidurkio/medianos išvedimą.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include <iomanip>
+#include <array>
+#include <algorithm>
 
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::array;
 
 
 struct Student {
     string first_name;
     string last_name;
-    unsigned scores[10]{0};
+
     unsigned num_of_scores{};
+    static constexpr unsigned max_num_of_scores{10};
+    array<unsigned, max_num_of_scores> scores{};
+
+    double score_average{};
+    double score_median{};
 };
 
 
 bool validate_input(string input);
-unsigned get_score();
 void ignore_line();
-void print_student(Student &student);
-
-
+void print_students(const std::array<Student, 10> &students, int num_of_students, unsigned option);
 
 
 int main()
@@ -30,9 +36,9 @@ int main()
     cout << "Įveskite studento vardą" << endl;
     string first_name;
 
-    unsigned num_of_students = 10;
-    unsigned current_student = 0;
-    Student students[num_of_students];
+    constexpr unsigned max_num_of_students = 10;
+    unsigned num_of_students = 0;
+    array<Student, max_num_of_students> students;
     Student student;
 
     do {
@@ -71,40 +77,78 @@ int main()
 
 
     cout << endl << "Veskite studento pažymius (paskutinis turi būti egzamino pažymys).\n"
-        << "Kai norėsite baigti įvedimą įveskite EOF (ctrl+z on windows, ctrl+d on Unix).\n"
+        << "Įveskite -1 jei norite baigti įvedimą\n"
         << "Pažymių kiekis neturi viršyti 10ies" << endl;
 
     unsigned score;
     int max_num_of_scores = 10;
-    int current_score = 0;
 
     while (true) {
 
         cout << "> ";
         cin >> score;
 
-        if (cin.eof()) {
-            ignore_line();
-            cout << endl;
-            break;
-        }
+        if (cin.fail() || (score < 1 || score > 10 || score == -1)) {
 
-        if (cin.fail() || (score < 1 || score > 10)) {
-            cout << "Įveskite skaičių nuo 1 iki 10" << endl;
             cin.clear();
             ignore_line();
 
+            if (score == -1) {
+
+                // Suvedus visus pažymius suskaičiuojam vidurkį ir medianą
+                double average{};
+                for (int i{}; i < student.num_of_scores; i++) {
+                    average += student.scores[i];
+                }
+                student.score_average = average / student.num_of_scores;
+
+                // Surikiuojam pažymius medianos apskaičiavimui
+                std::sort (student.scores.begin(), student.scores.begin() + student.num_of_scores);
+                
+                if (student.num_of_scores % 2 == 0) {
+                    int index2 = (int)student.num_of_scores / 2;
+                    int index1 = (int)student.num_of_scores / 2 - 1;
+                    double num_1 = student.scores[index1];
+                    double num_2 = student.scores[index2];
+                    student.score_median = (num_1 + num_2) / 2;
+                }
+                else {
+                    int temp = (int)student.num_of_scores / 2;
+                    student.score_median = student.scores[student.num_of_scores / 2];
+                }
+                
+                break;
+            }
+
+            cout << "Įveskite skaičių nuo 1 iki 10" << endl;
             continue;
-        }
-        else {
-            student.scores[current_score++] = score;
+
+        } else {
+            student.scores[student.num_of_scores++] = score;
             cout << endl;
         }
 
     } 
+    
+    students[num_of_students++] = student;
+    cout << "Pasirinkite ar norite apskaičiuoti vidurkį (1) ar medianą (2)?" << endl;
+    int option;
+    while (true) {
 
-    students[current_student++] = student;
-    print_student(students[0]);
+        cout << "> ";
+        cin >> option;
+
+        if (cin.fail() || (option != 1 && option != 2)) {
+            cout << "xdPasirinkite ar norite apskaičiuoti vidurkį (1) ar medianą (2)?" << endl;
+            cin.clear();
+            ignore_line(); 
+            continue;
+        }
+        
+        break;
+    }
+
+    print_students(students, num_of_students, option);
 
     return 0;
 }
@@ -134,16 +178,28 @@ bool validate_input(string input)
     return valid_string;
 }
 
-void print_student(Student &student)
+void print_students(const std::array<Student, 10> &students, int num_of_students, unsigned option)
 {
-    cout << student.first_name << " " << student.last_name << endl;
-    cout << "Pažymiai: ";
-    for (auto s : student.scores) {
-        if (s == 0)
-            break;
+    std::cout << std::setw(15) << std::left << "Pavardė" << std::setw(15) << std::left << "Vardas"
+        << std::left << "Galutinis ";
+    (option == 1) ? std::cout << "(Vid.)" : std::cout << "(Med).";
+    cout << endl << endl;
 
-        cout << s << " ";
+    std::string temp(50, '-');
+    cout << temp << endl;
+
+    for (int i{}; i < num_of_students; i++) {
+        std::cout << std::setw(15) << std::left << students[i].first_name;
+        std::cout << std::setw(15) << std::left << students[i].last_name;
+        
+        switch (option) {
+            case 1:
+                std::cout << students[i].score_average << std::endl << std::endl;
+                break; 
+
+            case 2:
+                std::cout << students[i].score_median << std::endl << std:: endl;
+                break; 
+        }
     }
-
-    cout << endl;
 }
