d5a80ed59e230aee111cf76ce26d0d31221259e1
dominykasmk <dominykas.makunas@mif.stud.vu.lt>
2022-04-05 22:53:05 +0300

    sufixinau buga jei atidarinejant faila kurio nera nemeta exceptiono

--- main.cpp
+++ main.cpp
@@ -3,131 +3,141 @@
 
 int main()
 {
+    while(true) {
 
-    unsigned option;
-    cout << "\nPasirinkite\n"
-        << "1 - Nuskaityti studentų duomenis iš failo\n"
-        << "2 - Įvesti studentų duomenis ranka\n" << endl;
+        unsigned option;
+        cout << "\nPasirinkite\n"
+            << "0 - baigti programą\n"
+            << "1 - Nuskaityti studentų duomenis iš failo\n"
+            << "2 - Įvesti studentų duomenis ranka\n" << endl;
 
-    while (true) {
-        cout << "> ";
-        cin >> option;
+        while (true) {
+            cout << "> ";
+            cin >> option;
 
-        if (cin && (option == 1 || option == 2)) {
-            break;         
-        }
-        else {
-            cout << "Įveskite 1 arba 2\n";
-            cin.clear();
-            ignore_line();
-        }
-    } 
+            if (cin && (option == 0 || option == 1 || option == 2)) {
+                break;         
+            }
+            else {
+                cout << "Įveskite 1 arba 2\n";
+                cin.clear();
+                ignore_line();
+            }
+        } 
 
-    switch (option) {
-        case 1: {
+        switch (option) {
 
-            ifstream student_file;
-            vector<Student> students;
+            case 0:
+                return 0;
 
-            student_file.exceptions( ifstream::badbit );
-            try {
-                student_file.open("kursiokai.txt");
+            case 1: {
 
-                if (student_file.is_open()) {
-                    string line;
+                ifstream student_file;
+                vector<Student> students;
+                const string file_name = "kursiokai.txt";
 
-                    getline(student_file, line);
-                    while (getline(student_file, line)) {
+                try {
+                    student_file.open(file_name);
 
-                            istringstream iss(line);
-                            Student student;
-                            
-                            iss >> student.last_name;
-                            iss >> student.first_name;
+                    if (student_file.fail())
+                        throw file_name;
 
-                            unsigned score;
-                            while (iss >> score) {
-                                student.scores.push_back(score);
+                    else if (student_file.is_open()) {
+                        string line;
+
+                        getline(student_file, line);
+                        while (getline(student_file, line)) {
+
+                                istringstream iss(line);
+                                Student student;
+                                
+                                iss >> student.last_name;
+                                iss >> student.first_name;
+
+                                unsigned score;
+                                while (iss >> score) {
+                                    student.scores.push_back(score);
+                                }
+
+                                calculate_averages(student);
+                                students.push_back(student);
+                        }
+                        
+                        cout << "\nPasirinkite ar norite apskaičiuoti vidurkį (1) ar medianą (2)?" << endl;
+                        unsigned option;
+                        while (true) {
+
+                            cout << "> ";
+                            cin >> option;
+                            if (cin && (option == 1 || option == 2)) {
+                                sort_students(students);
+                                print_students(students, option);
+                                break;
                             }
+                            else {
+                                cout << "Įveskite 1 arba 2\n";
+                                cin.clear();
+                                ignore_line();
+                            }
+                        }
+                    }
 
-                            calculate_averages(student);
-                            students.push_back(student);
+                    student_file.close();
+                }
+                catch (string e) {
+                    cout << "\nKlaida atidarinėjant failą\n" << endl;
+                    continue;
+                }
+
+                break;
+            }
+
+
+            case 2: {
+
+                vector<Student> students;
+                cout << "\nJeigu norite baigti įvestį, įveskite 'x'\n";
+
+                while (true) {
+
+                    Student student;
+                    if(input_student(&student) == -1) {
+                        break;
                     }
                     
-                    cout << "\nPasirinkite ar norite apskaičiuoti vidurkį (1) ar medianą (2)?" << endl;
-                    unsigned option;
-                    while (true) {
+                    calculate_averages(student);
+                    students.push_back(student);
+                }
 
-                        cout << "> ";
-                        cin >> option;
-                        if (cin && (option == 1 || option == 2)) {
-                            sort_students(students);
-                            print_students(students, option);
-                            break;
-                        }
-                        else {
-                            cout << "Įveskite 1 arba 2\n";
-                            cin.clear();
-                            ignore_line();
-                        }
+                if (students.empty()) {
+                    cout << "\nNeįvedėte nei vieno studento.\n" << endl;
+                    return 0;
+                }
+
+                cout << "\nPasirinkite ar norite apskaičiuoti vidurkį (1) ar medianą (2)?" << endl;
+                unsigned option;
+                while (true) {
+
+                    cout << "> ";
+                    cin >> option;
+                    if (cin && (option == 1 || option == 2)) {
+                        print_students(students, option);
+                        break;
+                    }
+                    else {
+                        cout << "Įveskite 1 arba 2\n";
+                        cin.clear();
+                        ignore_line();
                     }
                 }
-            }
-            catch (const ifstream::failure& e) {
-                cout << "Klaida atidarinėjant failą\n" << endl;
-                return 1;
-            }
-            student_file.close();
 
-            break;
+                break;
+            }
+
+            default:
+                break;
         }
-
-
-        case 2: {
-
-            vector<Student> students;
-            cout << "\nJeigu norite baigti įvestį, įveskite 'x'\n";
-
-            while (true) {
-
-                Student student;
-                if(input_student(&student) == -1) {
-                    break;
-                }
-                
-                calculate_averages(student);
-                students.push_back(student);
-            }
-
-            if (students.empty()) {
-                cout << "\nNeįvedėte nei vieno studento.\n" << endl;
-                return 0;
-            }
-
-            cout << "\nPasirinkite ar norite apskaičiuoti vidurkį (1) ar medianą (2)?" << endl;
-            unsigned option;
-            while (true) {
-
-                cout << "> ";
-                cin >> option;
-                if (cin && (option == 1 || option == 2)) {
-                    print_students(students, option);
-                    break;
-                }
-                else {
-                    cout << "Įveskite 1 arba 2\n";
-                    cin.clear();
-                    ignore_line();
-                }
-            }
-
-            break;
-        }
-
-        default:
-            break;
     }
-
 
     return 0;
 }
