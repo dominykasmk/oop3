935947c7ddc037f698aff13c36654a5d5ff7d83e
Dominykas Makūnas <dominykas.makunas@gmail.com>
2022-04-17 18:59:36 +0300

    pertvarkiau vektoriu skirstyma pirmai daliai

--- main.cpp
+++ main.cpp
@@ -8,11 +8,10 @@ int main()
         unsigned option;
         cout << "\nPasirinkite\n"
             << "0 - baigti programą\n"
-            << "1 - Įvesti studentų duomenis ranka\n"
-            << "2 - Sugeneruoti 5 studentų sąrašus automatiškai\n"
-            << "  21 - atlikti testus naudojant vektorių\n"
-            << "  22 - atliiti naudojant list'ą\n"
-            << "  23 - atlikti naudojant dek'ą\n"
+            << "1 - Sugeneruoti 5 studentų sąrašus automatiškai\n"
+            << "  11 - atlikti testus naudojant vektorių\n"
+            << "  12 - atliiti testus naudojant list'ą\n"
+            << "  13 - atlikti testus naudojant dek'ą\n"
 
             << endl;
 
@@ -20,7 +19,7 @@ int main()
             cout << "> ";
             cin >> option;
 
-            if (cin && (option == 0 || option == 1 || option == 2 || option == 21 || option == 22 || option == 23)) {
+            if (cin && (option == 0 || option == 1 || option == 11 || option == 12 || option == 13)) {
                 break;         
             }
             else {
@@ -36,47 +35,6 @@ int main()
                 return 0;
 
             case 1: {
-
-                vector<Student> students;
-                cout << "\nJeigu norite baigti įvestį, įveskite 'x'\n";
-
-                while (true) {
-
-                    Student student;
-                    if(input_student(&student) == -1) {
-                        break;
-                    }
-                    
-                    calculate_averages(student);
-                    students.push_back(student);
-                }
-
-                if (students.empty()) {
-                    cout << "\nNeįvedėte nei vieno studento.\n" << endl;
-                    return 0;
-                }
-
-                cout << "\nPasirinkite ar norite apskaičiuoti vidurkį (1) ar medianą (2)?" << endl;
-                unsigned option;
-                while (true) {
-
-                    cout << "> ";
-                    cin >> option;
-                    if (cin && (option == 1 || option == 2)) {
-                        print_students(students, option);
-                        break;
-                    }
-                    else {
-                        cout << "Įveskite 1 arba 2\n";
-                        cin.clear();
-                        ignore_line();
-                    }
-                }
-
-                break;
-            }
-
-            case 2: {
 
                 unsigned student_amount = 100;
                 unsigned scores_amount = 5;
@@ -97,7 +55,7 @@ int main()
                 break;
             }
 
-            case 21: {
+            case 11: {
                 
                 unsigned student_amount = 100;
                 string temp_file_name("studentai");
@@ -106,59 +64,44 @@ int main()
                     student_amount *= 10;
                     const string file_name = temp_file_name + std::to_string(student_amount) + ".txt";
 
-                    //reading
-                    ifstream student_file;
-                    vector<Student> student_vector;
+                    vector<Student> students;
+                    read_student_vector(students, file_name);
+                    vector<Student> students_high(students);
+
 
                     auto start = std::chrono::high_resolution_clock::now();
+                    std::sort(students_high.begin(), students_high.end(), compare_by_final_score);
+                    vector<Student> students_low;
 
-                    read_student_vector(student_vector, file_name);
+                    auto it = students_high.begin();
+                    auto end = students_high.end();
+
+                    for (; it != end; ++it) {
+                        if ((*it).final_score_avg >= 5)
+                            break;
+
+                        students_low.push_back(*it);
+                    }
+                    students_high.erase(students_high.begin(), it);
 
                     auto time = std::chrono::high_resolution_clock::now();
                     std::chrono::duration<double> diff = time - start;
                     auto stop = time;
 
-                    cout << endl << student_amount << "ies studentu vektoriaus nuskaitymas užtruko: "<< diff.count() << "s\n";
+                    cout << endl << student_amount << "ies studentų rūšiavimas naudojant vieną papildomą konteinerį užtruko: "<< diff.count() << "s\n";
 
-                    // sorting
-                    std::sort(student_vector.begin(), student_vector.end(), compare_by_final_score);
-                    
-                    vector<Student> students_low;
-                    vector<Student> students_high;
+                    write_student_file(students_high, "kietekai" + std::to_string(student_amount) + ".txt");
+                    write_student_file(students_low, "vargsiukai" + std::to_string(student_amount) + ".txt");
 
-                    auto it = student_vector.begin();
-                    auto end = student_vector.end();
-
-                    for (; it != end; it++) {
-                        
-                        if ((*it).final_score_avg >= 5) {
-                            break;
-                        }
-
-                        students_low.push_back(*it);
-                    }
-
-                    for (; it != end; it++) {
-                        
-                        students_high.push_back(*it);
-                    }
-
-                    time = std::chrono::high_resolution_clock::now();
-                    diff = time - stop;
-                    stop = time;
-
-                    cout << student_amount << "ies studentu vektoriaus rūšiavimas į dvi grupes užtruko: "<< diff.count() << "s\n";
-
-                    student_vector.clear();
-                    students_low.clear();
                     students_high.clear();
+                    students_low.clear();
                 }
 
                 break;
             }
 
 
-            case 22: {
+            case 12: {
                 
                 unsigned student_amount = 100;
                 string temp_file_name("studentai");
@@ -212,7 +155,7 @@ int main()
                 break;
             }
 
-            case 23: {
+            case 13: {
                 
                 unsigned student_amount = 100;
                 string temp_file_name("studentai");
