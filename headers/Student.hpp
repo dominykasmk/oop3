128011c5045175a669de894524bce3c5d073d14d
Dominykas Makūnas <dominykas.makunas@gmail.com>
2022-04-30 17:36:28 +0300

    Perėjau prie klasių implementacijos

--- headers/Student.hpp
+++ headers/Student.hpp
@@ -1,5 +1,5 @@
-#ifndef STUDENTS_HPP
-#define STUDENTS_HPP
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
 
 #include <iostream>
 #include <fstream>
@@ -28,7 +28,7 @@ using std::ifstream;
 using std::istringstream;
 
 
-struct Student {
+class Student {
     string first_name;
     string last_name;
 
@@ -40,18 +40,27 @@ struct Student {
 
     double final_score_avg{};
     double final_score_med{};
+
+public:
+    Student(std::string& data);
+    Student(std::string first_name, std::string last_name, unsigned test_score, std::vector<unsigned> scores);
+    void read_student(std::string& data);
+    void write_student(std::ostringstream& ss) const;
+    void calculate_averages();
+    void print_student() const;
+    unsigned get_score_amount() const;
+    string get_first_name() const;
+    string get_last_name() const;
+    double get_final_score() const;
 };
 
 
-bool validate_input(std::string input);
 void input_valid_string(std::string &input, const std::string prompt);
-void calculate_averages(Student &student);
 void ignore_line();
-int input_student(Student *student);
 void print_students(const std::vector<Student> &students, unsigned option);
-unsigned generate_random_score();
 bool compare(Student a, Student b);
 bool compare_by_final_score(Student a, Student b);
+unsigned generate_random_score();
 void sort_students(std::vector<Student> &students);
 void create_student_file(const std::string file_name, const unsigned student_amount, const unsigned scores_amount);
 void read_student_vector(std::vector<Student> &students, const std::string file_name);
