c68b4fede7bd0cfa9201b678c671227a3e9476ae
Dominykas Makūnas <dominykas.makunas@gmail.com>
2022-05-11 14:42:34 +0300

    Pridedu doxygen config'ą ir komentarus Studentų ir žmogaus klasei

---
+++ doc_pages/mainpage.md
@@ -0,0 +1,77 @@
+@mainpage Trečioji objektinio programavimo užduotis
+
+
+Ši programa aprašo studentų klasę. Leidžia kurti studentus, įvesti namų darbų ir egzaminų pažymius,
+skaičiuoti galutinius pažymius.
+Priklausomai nuo versijos leidžia paleisti įvairius testus matuojančius programos veikimo spartą
+naudojant vektorius, listus, dekus ir struktūras bei klases.
+
+# Versija 2.0
+
+Šioje programos versijoje pridėjau dokumentaciją ir unit testus
+
+## Kaip parunnint
+
+ - `git clone https://github.com/dominykasmk/oop3.git`
+ - `cd oop-test`
+ - `cmake -S . -B out/`
+ - `cd out/ && make`
+ - `./main`
+
+## Changelog
+ 
+ ---
+
+## [test](https://google.com)
+
+ 
+## [v0.1](https://github.com/dominykasmk/oop2/tree/v0.1) (22/02/2022)
+ - Implementuota naudojant dinaminius C masyvus ir vektorius
+## [v0.2](https://github.com/dominykasmk/oop2/tree/v0.2) (06/03/2022)
+ - Pridėta galimybė nuskaityti studentus iš failo
+ - Outpute studentai yra surikiuojami pagal pavardes
+## [v0.3](https://github.com/dominykasmk/oop2/tree/v0.3) (06/03/2022)
+ - Pridėtas exception'ų checkinimas atidarinėjant failą
+ - Kodas jau buvo splittintas į headerį, metodų source'ą ir main source failą
+ - Kode jau buvo naudojamos struktūros
+## [v0.4](https://github.com/dominykasmk/oop2/tree/v0.4) (05/04/2022)
+ - Pridėjau galimybę sugeneruoti studentų failus automatiškai
+ - Programa sugeneruoja 5 studentų failus, surušiuoja pagal pažymius ir pagal tai įšveda į du skirtingus failus
+ - Visi testai matuojami ir yra išvedama kiek kas laiko užėmė
+## [v0.5](https://github.com/dominykasmk/oop2/tree/v0.5) (06/04/2022)
+ - Implementuotas testas tikrinantis duomenų nuskaitymą iš failų ir rūšiavimą į dvi kategorijas naudojant vektorių, listą ir deką
+
+![](vector.png)
+![](list.png)
+![](deque.png)
+
+## [v1.0](https://github.com/dominykasmk/oop2/tree/v1.0) (17/04/2022)
+ - Ši versija matuoja kaip skiriasi studentų rušiavimas į dvi grupes naudojant vieną naują/du naujus konteinerius naudojant vektorių, list'ą arba dek'ą
+## [v1.1](https://github.com/dominykasmk/oop3/tree/v1.1) (30/04/2022)
+ - Pereita prie klasių vietoj struktūrų
+ - Palyginau programos veikimo spartą naudojant struktūras ir klases ir runninant testus su vektoriais ir 100000 ir 1000000 studentų failais
+
+### Struktūros
+![](struct.png)
+
+### Klasės
+![](class.png)
+
+ - Palyginau testų spartą naudojant O1, O2 ir O3 kompiliatoriaus optimizavimo flagus
+ 
+### OP1
+![](OP1.png)
+### OP2
+![](OP2.png)
+### OP3
+![](OP3.png)
+
+## [v1.2](https://github.com/dominykasmk/oop3/tree/v1.2) (08/05/2022)
+ - Pridėjau 'rule of three' konstriuktorius ir << ir >> operatorius studentų klasei
+ - Pridėjau porą meniu punktų demonstruojančių įvedimą ir išvedimą naudojant << ir >> operatorius
+
+## [v1.5](https://github.com/dominykasmk/oop3/tree/v1.5) (11/05/2022)
+ - Parašiau abstrakčią klasę Human iš kurios dabar paveldėja Student
+
+## [v2.0](https://github.com/dominykasmk/oop3/tree/v2.0) (11/05/2022)
+ - Pridėjau doxygen dokumentaciją
--- headers/Student.hpp
+++ headers/Student.hpp
@@ -28,20 +28,33 @@ using std::ifstream;
 using std::istringstream;
 
 
+/// Abstrakti žmogaus klasė
 class Human {
     protected:
         std::string first_name;
         std::string last_name;
 
     public:
+
+        /// Gražiną žmogaus vardą
         std::string get_first_name() const { return first_name; };
+
+        /// Gražina žmogaus pavardę
         std::string get_last_name() const { return last_name; };
         
+        /// Settina žmogaus vardą
         virtual void set_first_name(std::string name) = 0;
+
+        /// Settina žmogaus pavardę
         virtual void set_last_name(std:: string name) = 0;
 };
 
 
+/// Studentų klasė kurį paveldi iš abstrakčios klasės Human
+///
+/// Ši klasė leidžia sukurti Studento objektą kuriam galima
+/// priskirti vardą, pavardę, namų darbų pažymius, egzamino
+/// pažymį ir galima apskaičiuoti galutinį pažymį
 class Student : public Human {
     string first_name;
     string last_name;
@@ -59,39 +72,102 @@ public:
     Student() = default;
     Student(const Student&) = default;
     Student& operator = (const Student &) = default;
+
+    /// Konstruktorius kuriam galima pateikt visą eilutę su visais duomenimis kai nuskaitoma iš failo
+    ///
+    /// @param data Reference'as į duomenų string'ą
     Student(std::string& data);
+
+    /// Konstruktorius kuriam galima pateikt iškart visus duomenis apie studentą
+    ///
+    /// @param first_name Vardas
+    /// @param last_name Pavardė
+    /// @param test_score Egzamino pažymys
+    /// @param scores Pažymių vektorius
     Student(std::string first_name, std::string last_name, unsigned test_score, std::vector<unsigned> scores);
+
     ~Student() {};
     
+    /// Vardo setteris
+    /// @param first_name Studento vardas
     void set_first_name(std::string name) override { first_name = name; };
+
+    /// Pavardės setteris
+    /// @param last_name Studento pavardė
     void set_last_name(std::string name) override { last_name = name; };
 
+    /// Nuskaito duomenis apie studentą iš string'o
+    ///
+    /// @param data Reference'as į duomenų string'ą
     void read_student(std::string& data);
+
+    /// Išveda studento duomenis į stringstreamą
+    ///
+    /// @param ss stringstreamas į kurį bus išvesti duomenys
     void write_student(std::ostringstream& ss) const;
+
+    /// Apskaičiuoja studento galutinį pažymį
     void calculate_averages();
+
+    /// Išprintina duomenis apie studentą
     void print_student() const;
+
+    /// Gražina pažymių skaičių
     unsigned get_score_amount() const;
+
+    /// Gražina vardą
     string get_first_name() const;
+
+    /// Gražina pavardę
     string get_last_name() const;
+
+    /// Gražina galutinį pažymį
     double get_final_score() const;
+    
+    /// Gražiną pažymių vektorių
     const vector<unsigned>& get_scores() const;
 
+    /// << operatoriaus perdengimas
     friend std::ostream& operator<< (std::ostream& out, const Student& student);
+
+    /// >> operatoriaus perdengimas
     friend std::istream& operator>> (std::istream& in, Student& student);
 };
 
-
+/// Validuoja string'ą
 void input_valid_string(std::string &input, const std::string prompt);
+
+/// Ignoruoja nereikalingus duomenis buferyje
 void ignore_line();
+
+/// Išspausdina studentus
 void print_students(const std::vector<Student> &students, unsigned option);
+
+/// Palygina studentus pagal pavardę
 bool compare(Student a, Student b);
+
+/// Palygina studentus pagal galutinį pažymį
 bool compare_by_final_score(Student a, Student b);
+
+/// Sugeneruoja atsitiktinį pažymį
 unsigned generate_random_score();
+
+/// Surūšiuoja studentų vektorių
 void sort_students(std::vector<Student> &students);
+
+/// Sukuria studentų failą
 void create_student_file(const std::string file_name, const unsigned student_amount, const unsigned scores_amount);
+
+/// Nuskaito studentus iš failo į vektorių
 void read_student_vector(std::vector<Student> &students, const std::string file_name);
+
+/// Nuskaito studentus iš failo į list'ą
 void read_student_list(std::list<Student> &students, const std::string file_name);
+
+/// Nuskaito studentus iš failo į dek'ą
 void read_student_deque(std::deque<Student> &students, const std::string file_name);
+
+/// Išveda studentus į failą
 void write_student_file(const std::vector<Student> &students, const std::string file_name);
 
 #endif
